d6644c6e7066fcab1959852efbae9b10dc70c38e
acao <alc3pf@virginia.edu>
2020-09-13 00:58:31 -0400

    nav bar change

--- src/Navigation/NavigationMenu.js
+++ src/Navigation/NavigationMenu.js
@@ -9,7 +9,7 @@ function NavigationMenu() {
         <Navbar.Toggle aria-controls="main-navbar" />
         <Navbar.Collapse id="main-navbar" className="nav-options">
           <Nav>
-            <NavLink exact to="/" className="nav-option" activeClassName="nav-option--active">About</NavLink>
+            <NavLink exact to="/portfolio" className="nav-option" activeClassName="nav-option--active">About</NavLink>
             <NavLink to="/work" className="nav-option" activeClassName="nav-option--active">Work</NavLink>
             <NavLink to="/skills" className="nav-option" activeClassName="nav-option--active">Skills</NavLink>
             {/* <Nav.Link href="/documents" className="nav-option" activeClassName="underline">Documents</Nav.Link> */}
--- src/Navigation/Routes.js
+++ src/Navigation/Routes.js
@@ -4,15 +4,13 @@ import Home from '../Components/Home/Home';
 import Work from '../Components/Work/Work';
 import Contact from '../Components/Contact/Contact';
 import Skills from '../Components/Skills/Skills';
-import Documents from '../Components/Documents/Documents';
 
 const Routes = () => {
     return <Switch>
-            <Route path='/' component={Home} exact/>
+            <Route path='/portfolio' component={Home} exact/>
             <Route path='/work' component={Work}/>
             <Route path='/contact' component={Contact}/>
             <Route path='/skills' component={Skills}/>
-            {/* <Route path='/documents' component={Documents}/> */}
     </Switch>
     
 }
